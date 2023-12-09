653f3f5c596c0f43744b1b4303d4d3e6944df766
Thomas Krehbiel <tkrehbiel@gmail.com>
2023-12-08 21:07:41 -0500

    remove yarn 2 and husky

--- services/dev/init.sh
+++ services/dev/init.sh
@@ -1,15 +1,9 @@
-corepack enable
-yarn init -2
+npm pkg set scripts.build="npm install"
 npm pkg set scripts.run="node index.mjs"
 npm pkg set scripts.test="true"
-yarn add eslint --dev
+npm install eslint --save-dev
 npm pkg set scripts.lint="eslint"
-yarn add prettier --dev --exact
+npm install prettier --save-dev
 npm pkg set scripts.prettier="prettier --write ."
-yarn add husky --dev
-yarn husky install
-npm pkg set scripts.postinstall="husky install"
-yarn husky set .husky/pre-commit "yarn prettier && yarn lint && yarn test"
 cp ../dev/.prettierrc.json .
-#cp ../dev/.prettierignore .
 cp ../dev/.eslintrc.json .
